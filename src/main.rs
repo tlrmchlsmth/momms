6d6b1676e00961862f0112fd3f946d82f5c43498
Tyler Smith <tms@cs.utexas.edu>
2016-06-01 16:38:08 +0200

    Make Mat a trait,
    and make gemm nodes generic over different types of Mat<T>,
    so A, B, and C can be whatever types as long as they implement Mat<T>,
    (but they must all still be the same T for now)

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,39 @@ trait Scalar where
 impl Scalar for f64{}
 impl Scalar for f32{}
 
+trait Mat<T: Scalar> {
+    fn get( &self, y: usize, x: usize) -> T;
+    fn set( &mut self, y: usize, x: usize, alpha: T) -> ();
+    fn width( &self ) -> usize;
+    fn height( &self ) -> usize;
+
+    fn fill_rand( &mut self ) -> () {
+        for x in 0..self.width() {
+            for y in 0..self.height() {
+                self.set(y,x,rand::random::<T>());
+            }
+        }
+    }
+
+    fn fill_zero( &mut self ) -> () {
+        for x in 0..self.width() {
+            for y in 0..self.height() {
+                self.set(y,x,T::zero());
+            }
+        }
+    }
+
+    fn frosqr( &self ) -> T {
+        let mut norm:T = T::zero();
+        for x in 0..self.width() {
+            for y in 0..self.height() {
+                norm += self.get(y,x) * self.get(y,x);
+            }
+        }
+        return norm;
+    }
+}
+
 struct Matrix<T: Scalar> {
     h: usize,
     w: usize,
@@ -40,36 +73,22 @@ impl<T: Scalar> Matrix<T> {
         let mat = Matrix{ h: h, w: w, row_stride: 1, column_stride: h, buffer: buf };
         return mat;
     }
+}
+impl<T: Scalar> Mat<T> for Matrix<T> {
     fn get( &self, y: usize, x: usize) -> T {
         return self.buffer[y*self.row_stride + x*self.column_stride];
     }
     fn set( &mut self, y: usize, x: usize, alpha: T) -> () {
         self.buffer[y*self.row_stride + x*self.column_stride] = alpha;
     }
-    fn fill_rand( &mut self ) -> () {
-        for x in 0..self.w {
-            for y in 0..self.h {
-                self.set(y,x,rand::random::<T>());
-            }
-        }
+    fn width( &self ) -> usize {
+        return self.w;
     }
-    fn fill_zero( &mut self ) -> () {
-        for x in 0..self.w {
-            for y in 0..self.h {
-                self.set(y,x,T::zero());
-            }
-        }
-    }
-    fn frosqr( &self ) -> T {
-        let mut norm:T = T::zero();
-        for x in 0..self.w {
-            for y in 0..self.h {
-                norm += self.get(0,0) * self.get(0,0);
-            }
-        }
-        return norm;
+    fn height( &self ) -> usize {
+        return self.h;
     }
 }
+
 /*
 struct ColumnPanelMatrix<T: Scalar> {
     h: usize,
@@ -79,7 +98,7 @@ struct ColumnPanelMatrix<T: Scalar> {
     panel_stride: usize,
     buffer: Vec<T>,
 }
-impl<T: Scalar> Matrix<T> for ColumnPanelMatrix<T> {
+impl<T: Scalar> Mat<T> for ColumnPanelMatrix<T> {
     fn get( &self, x: usize, y:usize ) -> T {
         let panel_id = y / self.panel_w;
         let panel_index  = y % self.panel_w;
@@ -102,38 +121,30 @@ impl<T: Scalar> Matrix<T> for ColumnPanelMatrix<T> {
     }
 }*/
 
-struct View<T> {
+struct View {
     m: usize,
     n: usize,
     k: usize,
     m_off: usize,
     n_off: usize,
     k_off: usize,
-
-    a_pack: Vec<T>,
-    ap_orig_x: usize,
-    ap_orig_y: usize,
-    b_pack: Vec<T>,
-    bp_orig_x: usize,
-    bp_orig_y: usize,
-    c_pack: Vec<T>,
-    cp_orig_x: usize,
-    cp_orig_y: usize,
 }
-impl<T:Scalar> View<T>{
-    fn new( a: &Matrix<T>, b: &Matrix<T>, c: &Matrix<T> ) -> View<T>{
-        let v : View<T> = View{ m: c.h, n: c.w, k: a.w,
-                      m_off: 0, n_off: 0, k_off: 0,
-                      a_pack: Vec::new(), ap_orig_x: 0, ap_orig_y: 0,
-                      b_pack: Vec::new(), bp_orig_x: 0, bp_orig_y: 0,
-                      c_pack: Vec::new(), cp_orig_x: 0, cp_orig_y: 0 };
+impl View{
+    fn new<T: Scalar>( a: &Mat<T>, b: &Mat<T>, c: &Mat<T> ) -> View{
+        if a.height() != c.height() || a.width() != b.height() || c.width() != b.width() {
+            panic!("Cannot create View with nonconformal operands!");
+        }
+
+        let v : View = View{ m: c.height(), n: b.width(), k: a.width(),
+                      m_off: 0, n_off: 0, k_off: 0 };
         return v;
     }
 }
 
-trait GemmNode<T: Scalar> {
-     fn run( &self, a: &Matrix<T>, b: &Matrix<T>, c: &mut Matrix<T>, v: &mut View<T> ) -> ();
+trait GemmNode<T: Scalar, At: Mat<T>, Bt: Mat<T>, Ct: Mat<T>> {
+     fn run( &self, a: &At, b: &Bt, c: &mut Ct, v: &mut View ) -> ();
 }
+
 /*
 fn pack<T: Scalar>( a: &Matrix<T> ) {
 }
@@ -165,14 +176,18 @@ impl<T:Scalar, S:GemmNode<T>> GemmNode<T> for PackB<T,S> {
 }
 */
 
-struct PartM<T: Scalar, S: GemmNode<T>> {
+struct PartM<T: Scalar, At: Mat<T>, Bt: Mat<T>, Ct: Mat<T>, S: GemmNode<T, At, Bt, Ct>> {
     nb: usize,
     iota: usize,
     child: S,
-    _dt: PhantomData<T>
+    _t: PhantomData<T>,
+    _at: PhantomData<At>,
+    _bt: PhantomData<Bt>,
+    _ct: PhantomData<Ct>,
 }
-impl<T: Scalar, S: GemmNode<T>> GemmNode<T> for PartM<T,S> {
-    fn run( &self, a: &Matrix<T>, b: &Matrix<T>, c:&mut Matrix<T>, v: &mut View<T> ) -> () {
+impl<T: Scalar, At: Mat<T>, Bt: Mat<T>, Ct: Mat<T>, S: GemmNode<T, At, Bt, Ct>>
+    GemmNode<T, At, Bt, Ct> for PartM<T,At,Bt,Ct,S> {
+    fn run( &self, a: &At, b: &Bt, c:&mut Ct, v: &mut View ) -> () {
         let m_save = v.m;
         let m_off_save = v.m_off;
         
@@ -189,16 +204,17 @@ impl<T: Scalar, S: GemmNode<T>> GemmNode<T> for PartM<T,S> {
     }
 }
 
-//leaf node that does nothing for testing
+//Leaf node that does nothing. For testing.
 struct End { }
-impl<T: Scalar> GemmNode<T> for End {
-    fn run( &self, a: &Matrix<T>, b: &Matrix<T>, c: &mut Matrix<T>, v: &mut View<T> ) -> () {
+impl<T: Scalar, At: Mat<T>, Bt: Mat<T>, Ct: Mat<T>> GemmNode<T, At, Bt, Ct> for End {
+    fn run( &self, a: &At, b: &Bt, c: &mut Ct, v: &mut View ) -> () {
     }
 }
 
 struct TripleLoopKernel{}
-impl<T: Scalar> GemmNode<T> for TripleLoopKernel {
-    fn run( &self, a: &Matrix<T>, b: &Matrix<T>, c: &mut Matrix<T>, v: &mut View<T> ) -> () {
+impl<T: Scalar, At: Mat<T>, Bt: Mat<T>, Ct: Mat<T>> 
+    GemmNode<T, At, Bt, Ct> for TripleLoopKernel {
+    fn run( &self, a: &At, b: &Bt, c: &mut Ct, v: &mut View ) -> () {
         //For now, let's do an axpy based gemm
         for jn in 0..v.n {
             for pk in 0..v.k {
@@ -214,11 +230,11 @@ impl<T: Scalar> GemmNode<T> for TripleLoopKernel {
     }
 }
 
-fn axpy<T:Scalar>( alpha: T, x: &Matrix<T>, beta: T, y: &mut Matrix<T> ) {
+fn axpy<T:Scalar, Xt: Mat<T>, Yt: Mat<T>>( alpha: T, x: &Xt, beta: T, y: &mut Yt ) {
     //x.h should equal y.h and x.w should equal y.w!
     //but we dont check this yet
-    for i in 0..x.w {
-        for j in 0..x.h {
+    for i in 0..x.width() {
+        for j in 0..x.height() {
             let t = alpha * x.get(j,i) + beta * y.get(j,i);
             y.set(j,i,t); 
         }
@@ -229,7 +245,7 @@ fn main() {
     let mut a : Matrix<f64> = Matrix::new(10, 10);
     let mut b : Matrix<f64> = Matrix::new(10, 10);
     let mut c : Matrix<f64> = Matrix::new(10, 10);
-    let mut gemm_view : View<f64> = View::new(&a,&b,&c);
+    let mut gemm_view : View = View::new(&a,&b,&c);
     a.fill_rand();
     b.fill_rand();
     c.fill_zero();
@@ -250,13 +266,13 @@ fn main() {
     kernel.run( &a, &b, &mut c, &mut gemm_view );
 
     //Do bw = Bw, then abw = A*(Bw)   
-    let mut bw_view : View<f64> = View::new(&b, &w, &bw);
+    let mut bw_view : View = View::new(&b, &w, &bw);
     kernel.run( &b, &w, &mut bw, &mut bw_view );
-    let mut abw_view : View<f64> = View::new(&a, &bw, &abw);
+    let mut abw_view : View = View::new(&a, &bw, &abw);
     kernel.run( &a, &bw, &mut abw, &mut abw_view );
 
     //Do cw = Cw
-    let mut cw_view : View<f64> = View::new(&c, &w, &cw);
+    let mut cw_view : View = View::new(&c, &w, &cw);
     kernel.run( &c, &w, &mut cw, &mut cw_view );
     
     //Cw -= abw
