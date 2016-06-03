3635fd7381a397dd47cdbc42b46aea5bddcb3be2
Tyler Smith <tms@cs.utexas.edu>
2016-06-03 15:10:03 +0200

    Can print matrices now

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate rand;
 use std::ops::{Add, Mul, Sub, Div, AddAssign, MulAssign, SubAssign, DivAssign};
 use std::num::{Zero};
 use core::fmt::{Display};
-use core::marker::PhantomData;
+use core::marker::{PhantomData, Sized};
 
 trait Scalar where
     Self: Add<Self, Output=Self>,
@@ -28,9 +28,39 @@ impl Scalar for f32{}
 
 trait Mat<T: Scalar> {
     fn get( &self, y: usize, x: usize) -> T;
-    fn set( &mut self, y: usize, x: usize, alpha: T) -> ();
+    fn set( &mut self, y: usize, x: usize, alpha: T) -> ()
+            where T: Sized;
+    
     fn width( &self ) -> usize;
     fn height( &self ) -> usize;
+
+    fn printWolfram( &self ) -> () {
+        print!("{{");
+        for y in 0..self.height() {
+            print!("{{");
+            for x in 0..self.width() {
+                let formatted_number = format!("{:.*}", 2, self.get(y,x));
+                print!("{}", formatted_number);
+                if x < self.height() - 1 {
+                    print!(",");
+                }
+            }
+            print!("}}");
+            if y < self.width() - 1 {
+                print!(",");
+            }
+        }
+        print!("}}");
+    }
+
+    fn print( &self ) -> () {
+        for y in 0..self.width() {
+            for x in 0..self.height() {
+                let formatted_number = format!("{:.*}", 2, self.get(y,x));
+                print!("{} ", formatted_number);
+            }
+        }
+    }
 
     fn fill_rand( &mut self ) -> () {
         for x in 0..self.width() {
@@ -315,25 +345,31 @@ fn axpy<T:Scalar, Xt: Mat<T>, Yt: Mat<T>>( alpha: T, x: &Xt, beta: T, y: &mut Yt
 }
 
 fn main() {
-    let mut a : Matrix<f64> = Matrix::new(10, 10);
-    let mut b : Matrix<f64> = Matrix::new(10, 10);
-    let mut c : Matrix<f64> = Matrix::new(10, 10);
+    let m = 2;
+    let n = 2;
+    let k = 2;
+
+    let mut a : Matrix<f64> = Matrix::new(m, k);
+    let mut b : Matrix<f64> = Matrix::new(k, n);
+    let mut c : Matrix<f64> = Matrix::new(m, n);
     let mut gemm_view : View = View::new(&a,&b,&c);
     a.fill_rand();
     b.fill_rand();
     c.fill_zero();
 
 
-    let mut w : Matrix<f64> = Matrix::new(10, 1);
-    let mut abw : Matrix<f64> = Matrix::new(10, 1);
-    let mut bw : Matrix<f64> = Matrix::new(10, 1);
-    let mut cw : Matrix<f64> = Matrix::new(10, 1);
+    let mut w : Matrix<f64> = Matrix::new(n, 1);
+    let mut bw : Matrix<f64> = Matrix::new(k, 1);
+    let mut abw : Matrix<f64> = Matrix::new(m, 1);
+    let mut cw : Matrix<f64> = Matrix::new(m, 1);
     w.fill_rand();
     cw.fill_zero();
     bw.fill_zero();
     abw.fill_zero();
 
- 
+
+    //You can be explicit with the types like here:
+    //The problem is that the types grow quadratically with the number of steps in the control tree!
     let kernel : TripleLoopKernel = TripleLoopKernel{};
     let loop1 : PartM<f64, Matrix<f64>, Matrix<f64>, Matrix<f64>, TripleLoopKernel> = PartM::new(5, 1, kernel);
     let loop2 : PartK<f64, Matrix<f64>, Matrix<f64>, Matrix<f64>, 
@@ -342,9 +378,28 @@ fn main() {
                 PartK<f64, Matrix<f64>, Matrix<f64>, Matrix<f64>, 
                 PartM<f64, Matrix<f64>, Matrix<f64>, Matrix<f64>, TripleLoopKernel>>> = PartN::new(5, 1, loop2);
 
+    //If you instead delcare the phantom data here, the type inference system works
+    //I.E. the compiler can infer the type of loop1_inf when you use that type in loop2_inf
+    //Now the code grows linearly with the number of steps in our control tree
+    //The problem with this is that we have all this ugly phantom data laying around!
+    let kernel_inf = TripleLoopKernel{};
+    let loop1_inf = PartM{ bsz: 5, iota: 1, child: kernel_inf, 
+        _t: PhantomData::<f64>, _at: PhantomData::<Matrix<f64>>, _bt: PhantomData::<Matrix<f64>>, _ct: PhantomData::<Matrix<f64>> };
+    let loop2_inf = PartK{ bsz: 5, iota: 1, child: loop1_inf, 
+        _t: PhantomData::<f64>, _at: PhantomData::<Matrix<f64>>, _bt: PhantomData::<Matrix<f64>>, _ct: PhantomData::<Matrix<f64>> };
+    let loop3_inf = PartN{ bsz: 5, iota: 1, child: loop2_inf, 
+        _t: PhantomData::<f64>, _at: PhantomData::<Matrix<f64>>, _bt: PhantomData::<Matrix<f64>>, _ct: PhantomData::<Matrix<f64>> };
+
     let ref_gemm : TripleLoopKernel = TripleLoopKernel{};
     //C = AB
     loop3.run( &a, &b, &mut c, &mut gemm_view );
+
+    a.printWolfram();
+    println!("");
+    b.printWolfram();
+    println!("");
+    c.printWolfram();
+    println!("");
 
     //Do bw = Bw, then abw = A*(Bw)   
     let mut bw_view : View = View::new(&b, &w, &bw);
@@ -360,11 +415,4 @@ fn main() {
     axpy( 1.0, &abw, -1.0, &mut cw );
     let norm = cw.frosqr();
     println!("diff is {}", norm);
-    /*
-
-    let leaf : End = End{};
-    let root : PartM<End> = PartM{child: leaf};
-    root.run(&a, &b, &mut c);
-
-    println!("c is {}", c.buffer);*/
 }
