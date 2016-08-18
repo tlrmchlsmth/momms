661ada9699f4f9c177c960a0edbe52ed4bbbf7dc
Tyler Michael Smith <tms@cs.utexas.edu>
2016-08-18 16:16:05 -0500

    More intelligently look for blis

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use std::io::Write;
 use std::path::Path;*/
 
 fn main() -> () {
-    println!("cargo:rustc-link-search=native=/Users/tyler/blis/lib");
+    println!("cargo:rustc-link-search=native={}/blis/lib", std::env::home_dir().unwrap().to_str().unwrap());
     println!("cargo:rustc-link-search=native=/usr/local/lib");
     println!("cargo:rustc-link-lib=static=blis");
 }
