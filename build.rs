abfbb270d0f4bed789012c7e7d1d936dd56d7786
Tyler Michael Smith <tms@cs.utexas.edu>
2017-05-01 16:36:04 -0500

    change build.rs so it links to libgomp.so on ubuntu

--- build.rs
+++ build.rs
@@ -7,6 +7,11 @@ fn main() -> () {
     println!("cargo:rustc-link-search=native={}/blis/lib", std::env::home_dir().unwrap().to_str().unwrap() );
     println!("cargo:rustc-link-search=native=/usr/local/lib");
     println!("cargo:rustc-link-lib=static=blis");
+// 
+//    println!("cargo:rustc-link-search=native=/usr/lib/gcc/x86_64-linux-gnu/5");
+//    println!("cargo:rustc-link-lib=static=gomp");
+    println!("cargo:rustc-link-search=native=/usr/lib/gcc/x86_64-linux-gnu/5");
+    println!("cargo:rustc-link-lib=dylib=gomp");
 
 //  The following is needed for compiling with mkl
 //    println!("cargo:rustc-link-lib=static=irc");
